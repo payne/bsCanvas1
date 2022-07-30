e033ed2c37174ce8dc439d88ef1f681821f91c6d
Matt Payne <Payne@MattPayne.org>
2022-07-29 19:45:28 -0500

    ng generate component canvas1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Canvas1Component } from './canvas1/canvas1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Canvas1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/canvas1/canvas1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas1',
+  templateUrl: './canvas1.component.html',
+  styleUrls: ['./canvas1.component.scss']
+})
+export class Canvas1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
